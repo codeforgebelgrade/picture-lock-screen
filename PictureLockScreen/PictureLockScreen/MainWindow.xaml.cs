0d6b95ed6e73d014653c032da38710acfc68bb89
Miroslav Lazovic <miroslav.lazovich@outlook.com>
2017-09-27 22:29:46 +0200

    Added handling for FileNotFound exception

--- PictureLockScreen/PictureLockScreen/MainWindow.xaml.cs
+++ PictureLockScreen/PictureLockScreen/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Runtime.Serialization.Json;
-using System.Configuration;
 using Codeforge.PictureLockCommons;
 using System.IO;
-using System.Text;
 using static Codeforge.PictureLockCommons.PictureLockList;
 
 namespace PictureLockScreen
@@ -24,19 +21,31 @@ namespace PictureLockScreen
         {
             InitializeComponent();
 
-            String settingsFilepath = System.Configuration.ConfigurationManager.AppSettings["jsonFilePath"];
-
-            var jsonSerializer = new DataContractJsonSerializer(typeof(PictureLockList));
-            object objResponse = jsonSerializer.ReadObject(File.OpenRead(settingsFilepath + "images.json"));
-            PictureLockList pictureLockList = objResponse as PictureLockList;
-
-            List<PictureLockListItem> pictureLockItems = pictureLockList.PictureLockItems;
-            Random random = new Random();
-            int index = random.Next(0, pictureLockItems.Count);
-            currentPicture = pictureLockItems[index];
-            image.Source = new BitmapImage(new Uri(pictureLockItems[index].FilePath));
+            try
+            {
+                String settingsFilepath = System.Configuration.ConfigurationManager.AppSettings["jsonFilePath"];
+                var jsonSerializer = new DataContractJsonSerializer(typeof(PictureLockList));
+                object objResponse = jsonSerializer.ReadObject(File.OpenRead(settingsFilepath + "images.json"));
+                PictureLockList pictureLockList = objResponse as PictureLockList;
+                List<PictureLockListItem> pictureLockItems = pictureLockList.PictureLockItems;
+                Random random = new Random();
+                int index = random.Next(0, pictureLockItems.Count);
+                currentPicture = pictureLockItems[index];
+                image.Source = new BitmapImage(new Uri(pictureLockItems[index].FilePath));
+            } 
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Settings file is missing!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Close();
+            }
+            
         }
 
+        /// <summary>
+        /// Compares the user input to the passpharase of the corresponding picture
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string passphrase = this.tbAnswer.Text;
